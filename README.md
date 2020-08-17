b799f88119cee3ceedc46eb3ff8154b6c5aac5a3
Laith Shono <lshono8@gmail.com>
2020-08-17 04:57:42 +0300

    finished the logobar and the recent connection

--- README.md
+++ README.md
@@ -1,16 +1,10 @@
-# vpn_app
+# VPN app template
 
-A new Flutter project.
+a template for a VPN app
+NOT a working app
 
-## Getting Started
+recreation of [Mohammad Reza Farahzad's design](https://dribbble.com/shots/14028358-VPN-App-Ui-Design?utm_source=Clipboard_Shot&utm_campaign=mrfarahzad&utm_content=VPN%20App%20Ui%20Design&utm_medium=Social_Share)
 
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
-
-For help getting started with Flutter, view our
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+## resources
+- [Mohammad Reza Farahzad's design](https://dribbble.com/shots/14028358-VPN-App-Ui-Design?utm_source=Clipboard_Shot&utm_campaign=mrfarahzad&utm_content=VPN%20App%20Ui%20Design&utm_medium=Social_Share)
+- [flat flags](https://github.com/wobblecode/flat-flags)
--- lib/main.dart
+++ lib/main.dart
@@ -1,117 +1,208 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_icons/flutter_icons.dart';
+import 'package:vpn_app/constants.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
+        primarySwatch: Colors.yellow
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  bool isOn;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  void initState() {
+    super.initState();
+    isOn = true;
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+      backgroundColor: isOn? kYellowColor: kGrayColor,
+      
+      body: Column(
+        children: [
+
+          // Logo bar
+          Padding(
+            padding: const EdgeInsets.only(top: 40,left: 30, right: 30),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+
+                Text("VPN",
+                  style: TextStyle(
+                    fontWeight: FontWeight.w900,
+                    fontSize: 25,
+                    color: isOn? kGrayColor: Colors.white,
+                  ),
+                ),
+                
+                Expanded(child: Container()),
+
+                // Premium Button
+                Material(
+                  color: isOn? Color(0x66000000): kDarkSurfaceColor,
+                  borderRadius: BorderRadius.circular(15),
+                  child: InkWell(
+                    borderRadius: BorderRadius.circular(15),
+                    onTap: (){},
+                    child: Container(
+                      padding: EdgeInsets.symmetric(vertical: 11, horizontal: 15),
+                      child: Row(
+                        children: [
+                          Icon(MaterialCommunityIcons.crown,
+                            color: Colors.yellow
+                          ),
+                          Text(" Go Premium",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.w500
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+          ), // logo bar
+
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 40),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left:30),
+                  child: Text("Recent Connection",
+                    style: TextStyle(
+                      color: isOn? kGrayColor: Colors.white,
+                      fontWeight: FontWeight.w500
+                    ),
+                  ),
+                ),
+                SizedBox(height: 15),
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  padding: EdgeInsets.only(bottom: 5),
+                  child: Row(
+                    children: _buildConnections()
+                  ),
+                )
+              ],
             ),
-          ],
-        ),
+          )
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+
+  List<Widget> _buildConnections(){
+    const countries = [
+      "United States",
+      "United Kingdom",
+      "Germany",
+      "Japan",
+    ];
+    List list = new List<Widget>((countries.length*2));
+
+    list[0] = SizedBox(width: 30);
+
+    for (var i = 1; i < list.length; i++) {
+
+      list[i]= Material(
+        elevation: isOn? i==1? 3:0 : 0,
+        borderRadius: BorderRadius.circular(15),
+        color: isOn? i==1? Colors.white: Color(0x15000000): kDarkSurfaceColor,
+        child: InkWell(
+          borderRadius: BorderRadius.circular(15),
+          onTap: isOn && i==1? null: (){},
+          child: Container(
+            width: 150,
+            height: 100,
+            padding: EdgeInsets.symmetric(horizontal: 20,vertical: 15),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+
+                // Flag
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 8.0),
+                  child: Image.asset("assets/flags/${countries[i~/2]}.png",scale: 2),
+                ),
+
+                // Country name
+                Text(
+                  countries[i~/2],
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 14,
+                    color: isOn? i==1? Colors.black: kGrayColor: Colors.white
+                  ),
+                ),
+
+                // Connection status
+                Padding(
+                  padding: const EdgeInsets.only(top: 5),
+                  child: isOn && i==1?
+                  Row(
+                    children: [
+                      Icon(MaterialCommunityIcons.shield_check_outline,
+                        size: 16,
+                        color: Color(0xFF1abb1d),
+                      ),
+                      Text("Connected",
+                        style: TextStyle(
+                          fontSize: 13,
+                          color: Color(0xFF1abb1d),
+                          fontWeight: FontWeight.bold
+                        ),
+                      )
+                    ],
+                  )
+                  :Text("Connect",
+                    style: TextStyle(
+                      fontSize: 13,
+                      fontWeight: FontWeight.w500,
+                      color: isOn? Colors.black: Colors.yellow[700]
+                    )
+                  ),
+                )
+              ],
+            )
+          )
+        ),
+      );
+
+      if(i != list.length-1) list[++i] = SizedBox(width: 10);
+
+    }
+    return list;
+  }
+
 }
