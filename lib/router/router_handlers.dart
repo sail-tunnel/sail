2caf73034d7c80b3cb6bfed1265bc094d3e5a08d
losgif <losgif@gmail.com>
2022-05-05 08:58:03 +0800

    docs: add routes description

--- lib/router/router_handlers.dart
+++ lib/router/router_handlers.dart
@@ -10,45 +10,49 @@ import 'package:sail_app/pages/server_list.dart';
 import 'package:sail_app/pages/webview_widget.dart';
 import 'dart:convert';
 
-//入口
+/// 入口
 Handler rootHandler = Handler(
     handlerFunc: (BuildContext context, Map<String, List<String>> parameters) {
       return MainPage();
     });
 
-//引导页
+/// 引导页
 Handler splashHandler = Handler(
     handlerFunc: (BuildContext context, Map<String, List<String>> parameters) {
       return GuidePage();
     });
 
-//首页
+/// 首页
 Handler homeHandler = Handler(
     handlerFunc: (BuildContext context, Map<String, List<String>> parameters) {
       return HomePage();
     });
 
-//404页面
+/// 404页面
 Handler notFindHandler = Handler(
     handlerFunc: (BuildContext context, Map<String, List<String>> parameters) {
       return NotFindPage();
     });
 
+/// 登录页
 Handler loginHandler = Handler(
     handlerFunc: (BuildContext context, Map<String, List<String>> parameters) {
       return LoginPage();
     });
 
+/// 套餐页
 Handler planHandle = Handler(
     handlerFunc: (BuildContext context, Map<String, List<String>> parameters) {
       return PlanPage();
     });
 
+/// 服务器节点页
 Handler serverListHandler = Handler(
     handlerFunc: (BuildContext context, Map<String, List<String>> parameters) {
       return ServerListPage();
     });
 
+/// WebView页
 Handler webViewHandler = Handler(
     handlerFunc: (BuildContext context, Map<String, List<String>> parameters) {
       var title = jsonDecode(parameters["titleName"].first);
