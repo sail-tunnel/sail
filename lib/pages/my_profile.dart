267fcb689b8d9b7939a3a3f683904ade2b5cc91b
losgif <losgif@gmail.com>
2022-07-03 19:15:45 +0800

    feat: add my profile and settings page

--- lib/pages/my_profile.dart
+++ lib/pages/my_profile.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:provider/provider.dart';
+import 'package:sail_app/models/user_model.dart';
+import 'package:sail_app/utils/navigator_util.dart';
+import 'package:sail_app/widgets/ProfileWidget.dart';
 
 class MyProfile extends StatefulWidget {
   const MyProfile({Key key}) : super(key: key);
@@ -8,13 +13,269 @@ class MyProfile extends StatefulWidget {
 }
 
 class MyProfileState extends State<MyProfile> {
+  UserModel _userModel;
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    _userModel = Provider.of<UserModel>(context);
+  }
+
+  void onLogoutTap() {
+    _userModel.logout();
+    NavigatorUtil.goLogin(context);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Text('MyProfile'),
-        Text('MyProfile'),
-      ],
+    return Container(
+      padding: EdgeInsets.only(
+          right: ScreenUtil().setWidth(32),
+          left: ScreenUtil().setWidth(32),
+          top: ScreenUtil().setHeight(32),
+          bottom: ScreenUtil().setHeight(32)),
+      child: Material(
+        borderRadius: BorderRadius.circular(16),
+        child: Padding(
+          padding: const EdgeInsets.only(top: 24, bottom: 24),
+          child: Column(
+            children: <Widget>[
+              ProfileWidget(
+                avatar: _userModel.userEntity?.avatarUrl,
+                userName: _userModel?.userEntity?.email ?? "Username",
+                onTap: onLogoutTap,
+              ),
+              Container(
+                padding: const EdgeInsets.only(top: 24, bottom: 24),
+                child: Container(
+                  decoration: BoxDecoration(
+                    border: Border(bottom: BorderSide(color: Colors.grey[300])),
+                  ),
+                ),
+              ),
+              const AccountWidget(),
+              Container(
+                padding: const EdgeInsets.only(top: 24, bottom: 24),
+                child: Container(
+                  decoration: BoxDecoration(
+                    border: Border(bottom: BorderSide(color: Colors.grey[300])),
+                  ),
+                ),
+              ),
+              const TicketWidget(),
+              Container(
+                padding: const EdgeInsets.only(top: 24, bottom: 24),
+                child: Container(
+                  decoration: BoxDecoration(
+                    border: Border(bottom: BorderSide(color: Colors.grey[300])),
+                  ),
+                ),
+              ),
+              const StatisticsWidget()
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class AccountWidget extends StatelessWidget {
+  const AccountWidget({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.only(left: 24, right: 24),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Container(
+            padding: const EdgeInsets.only(bottom: 16),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: const [
+                Text(
+                  "账户",
+                  style: TextStyle(
+                    color: Color(0xFFADADAD),
+                    fontSize: 20,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.only(bottom: 16),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: const [
+                Text(
+                  "💰 钱包",
+                  style: TextStyle(
+                    fontSize: 18,
+                  ),
+                ),
+                Icon(Icons.arrow_forward_ios, size: 16),
+              ],
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.only(bottom: 16),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: const [
+                Text(
+                  "🔑 修改密码",
+                  style: TextStyle(
+                    fontSize: 18,
+                  ),
+                ),
+                Icon(Icons.arrow_forward_ios, size: 16),
+              ],
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.only(bottom: 16),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: const [
+                Text(
+                  "📧 邮件通知",
+                  style: TextStyle(
+                    fontSize: 18,
+                  ),
+                ),
+                Icon(Icons.arrow_forward_ios, size: 16),
+              ],
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.only(bottom: 16),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: const [
+                Text(
+                  "🔒 绑定第三方账号",
+                  style: TextStyle(
+                    fontSize: 18,
+                  ),
+                ),
+                Icon(Icons.arrow_forward_ios, size: 16),
+              ],
+            ),
+          ),
+          Container(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: const [
+                Text(
+                  "🛡️ 重置订阅信息",
+                  style: TextStyle(
+                    fontSize: 18,
+                  ),
+                ),
+                Icon(Icons.arrow_forward_ios, size: 16),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class TicketWidget extends StatelessWidget {
+  const TicketWidget({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.only(left: 24, right: 24),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Container(
+            padding: const EdgeInsets.only(bottom: 16),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: const [
+                Text(
+                  "工单",
+                  style: TextStyle(
+                    color: Color(0xFFADADAD),
+                    fontSize: 20,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.only(bottom: 16),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: const [
+                Text(
+                  "🎫 我的工单",
+                  style: TextStyle(
+                    fontSize: 18,
+                  ),
+                ),
+                Icon(Icons.arrow_forward_ios, size: 16),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class StatisticsWidget extends StatelessWidget {
+  const StatisticsWidget({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.only(left: 24, right: 24),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Container(
+            padding: const EdgeInsets.only(bottom: 16),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: const [
+                Text(
+                  "统计",
+                  style: TextStyle(
+                    color: Color(0xFFADADAD),
+                    fontSize: 20,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.only(bottom: 16),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: const [
+                Text(
+                  "🔖 流量明细",
+                  style: TextStyle(
+                    fontSize: 18,
+                  ),
+                ),
+                Icon(Icons.arrow_forward_ios, size: 16),
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
---
+++ lib/widgets/ProfileWidget.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class ProfileWidget extends StatelessWidget {
+  const ProfileWidget({Key key, @required this.userName, @required this.avatar, @required this.onTap})
+      : assert(userName != null),
+        assert(avatar != null),
+        assert(onTap != null),
+        super(key: key);
+
+  final String avatar;
+  final String userName;
+  final Function onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.only(right: 24, left: 24),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            children: <Widget>[
+              ClipOval(child: Image(image: NetworkImage(avatar), width: 40, height: 40)),
+              Padding(
+                padding: const EdgeInsets.only(left: 12),
+                child: Text(
+                  userName,
+                  style: const TextStyle(fontSize: 18, fontWeight: FontWeight.w500, color: Colors.black),
+                ),
+              )
+            ],
+          ),
+          Material(
+            color: const Color(0x66000000),
+            borderRadius: BorderRadius.circular(30),
+            child: InkWell(
+              onTap: onTap,
+              child: Container(
+                padding: const EdgeInsets.symmetric(
+                    vertical: 5, horizontal: 10),
+                child: const Text(
+                  '退出',
+                  style: TextStyle(
+                      fontSize: 14, color: Colors.white, fontWeight: FontWeight.w500),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
