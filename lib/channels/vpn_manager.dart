2821f3d7698555d829112d0efb850ada076d2f4a
cyril-cheng <49748357+cyril-cheng@users.noreply.github.com>
2023-04-30 03:41:45 +0800

    Update vpn_manager.dart

--- lib/channels/vpn_manager.dart
+++ lib/channels/vpn_manager.dart
@@ -13,7 +13,7 @@ enum VpnStatus {
 class VpnManager {
   Future<VpnStatus> getStatus() async {
     // Native channel
-    const platform = MethodChannel("com.prosfinityx.ang/vpn_manager");
+    const platform = MethodChannel("com.sail_tunnel.sail/vpn_manager");
     int result;
     try {
       result = await platform.invokeMethod("getStatus");
